d8a08ae090730492d09d3852fa1003fe43e3d729
Sebastian Ireland <irelandsh10@gmail.com>
2024-08-05 15:54:33 -0400

    Cambios Header, Barra de busqueda

--- view/index.php
+++ view/index.php
@@ -8,8 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-
 </head>
 
 <body>
@@ -18,18 +18,7 @@
             <nav class="navbar shadow-sm bg-light" id="navbar">
                 <div class="row w-100 align-items-center">
                     <div class="col d-flex justify-content-start px-4">
-                        <div class="btn-group me-2" role="group" aria-label="Basic example">
-                            <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="off" checked>
-                            <label class="btn btn-outline-primary" for="success-outlined">Estudiante</label>
-
-                            <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" autocomplete="off">
-                            <label class="btn btn-outline-primary" for="danger-outlined">Docente</label>
-                        </div>
-                        <div class="input-group me-2">
-                            <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
-                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                        </div>
-                        <button class="btn btn-primary me-2">Buscar</button>
+                        <button class="btn btn-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="bi bi-layout-sidebar-inset"></i></button>
                     </div>
                     <div class="col d-flex justify-content-end">
                         <h6 class="px-3 mb-0">Jaime David Guszman Rojas</h6>
@@ -38,15 +27,41 @@
             </nav>
         </header>
     </section>
-    <div class="container mt-4" id="container-main">
+    <div class="container mt-3" id="container-main">
         <!-- Primera y segunda fila combinadas -->
-        <div class="row mb-4">
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <div class="card shadow-sm mb-2 w-50">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-md-4 mb-2 mb-md-0">
+                                <div class="btn-group w-100" role="group" aria-label="Basic example">
+                                    <input type="radio" class="btn-check" name="options-outlined" id="success-outlined" autocomplete="off" checked>
+                                    <label class="btn btn-outline-primary w-50" for="success-outlined"><i class="fas fa-user-graduate"></i>
+                                    </label>
+
+                                    <input type="radio" class="btn-check" name="options-outlined" id="danger-outlined" autocomplete="off">
+                                    <label class="btn btn-outline-primary w-50" for="danger-outlined"><i class="fas fa-chalkboard-teacher"></i>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-8 mb-2 mb-md-0">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" placeholder="Rut" aria-label="Username" aria-describedby="basic-addon1">
+                                    <button class="btn btn-primary"><i class="bi bi-search"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class=" row mb-4 mt-1">
             <div class="col-md-6">
                 <div class="card shadow-sm h-100">
                     <h5 class="card-header">
                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                             <button class="btn btn-primary" type="button">Generar Alerta</button>
-                            <button class="btn btn-primary" type="button">Generar Gestion</button>
                         </div>
                     </h5>
                     <div class="card-body">
@@ -108,9 +123,10 @@
 
 
     <!-- Offcanvas Menu -->
-    <div class="offcanvas offcanvas-start show bg-mi-color" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+    <div class="offcanvas offcanvas-start bg-mi-color" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasScrollingLabel">logo</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
             <div class="d-grid gap-2">
